7628f2099b0eb9d2d3e192d05c331adfb1b144b6
CrgioPeca88 <crgiopeca88@gmail.com>
2020-11-24 12:01:53 -0500

    [FEATURE] VisibilityCHange event added - auto-pause.js file adjusted with visibilityCHange event - styles.css adjusted

--- src/plugins/auto-pause.js
+++ src/plugins/auto-pause.js
@@ -3,23 +3,43 @@
 function AutoPause() {
   this.threshold = 0.50;
   this.handleIntersection = this.handleIntersection.bind(this);
+  this.handleVisibilityChange = this.handleVisibilityChange.bind(this);
 }
 
 AutoPause.prototype.run = function(player) {
   this.player = player;
+  this.runApScroll();
+  this.runApTabs();
+}
+
+AutoPause.prototype.handleIntersection = function(entries) {
+  const entry = entries[0];
+  this.focusOk = this.threshold <= entry.intersectionRatio;
+  if(this.focusOk) {
+    this.player.play();
+  } else {
+    this.player.pause();
+  }
+}
+
+AutoPause.prototype.handleVisibilityChange = function() {
+  if(document.visibilityState === 'visible' && this.focusOk) {
+    this.player.play();
+  } else {
+    this.player.pause();
+  }
+}
+
+AutoPause.prototype.runApScroll = function() {
   const observer = new IntersectionObserver(this.handleIntersection, {
     threshold: this.threshold
   });
   observer.observe(this.player.media);
 }
 
-AutoPause.prototype.handleIntersection = function(entries) {
-  const entry = entries[0];
-  if(this.threshold <= entry.intersectionRatio) {
-    this.player.play();
-  } else {
-    this.player.pause();
-  }
+AutoPause.prototype.runApTabs = function() {
+  document.addEventListener('visibilitychange', this.handleVisibilityChange )
 }
+
 
 export default AutoPause;
